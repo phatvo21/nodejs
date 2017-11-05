9a3ce63b7e4de05b09dc16c76c2c73c3c7169465
Phat VT <phat.vt@3si.com.vn>
2017-11-05 20:36:45 +0700

    commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    googleClientID: '630356992104-qcqfuqkgqcr8m5t59m2srilj39f4an0s.apps.googleusercontent.com',
+    googleClientSecsret: 'aMx3YfAQysCk4NPi2Q2-t-8E',
+
+};
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
-
+require('./services/passport');
 const app = express();
 
-app.get('/', (req, res) => {
+//Client ID: 630356992104-qcqfuqkgqcr8m5t59m2srilj39f4an0s.apps.googleusercontent.com
+//Client secret: aMx3YfAQysCk4NPi2Q2-t-8E
 
-    res.send({ by: 'phat'});
-
-});
-
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
